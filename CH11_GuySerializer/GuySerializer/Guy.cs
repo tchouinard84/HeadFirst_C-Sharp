fcfa0c3fd725daced97265b1a091874df3baef61
tchouina <timothy.chouinard@emp-corp.com>
2017-11-22 16:33:26 -0500

    CH11 - working on serialization using DataContract

---
+++ CH11_GuySerializer/GuySerializer/Guy.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace GuySerializer
+{
+    [DataContract(Namespace = "HeadFirst_CSharp/CH11")]
+    public class Guy
+    {
+        [DataMember]
+        public string Name { get; private set; }
+
+        [DataMember]
+        public int Age { get; private set; }
+
+        [DataMember]
+        public decimal Cash { get; private set; }
+    }
+}
