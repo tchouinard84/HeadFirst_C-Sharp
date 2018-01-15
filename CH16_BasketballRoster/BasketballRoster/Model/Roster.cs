6c3af1596b772e6eda27925eff5a73953f8c6133
tchouina <timothy.chouinard@emp-corp.com>
2018-01-15 14:32:27 -0500

    CH16_BasketballRoster - steps 1-5 (added 2 model classes and the view)

---
+++ CH16_BasketballRoster/BasketballRoster/Model/Roster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace BasketballRoster.Model
+{
+    public class Roster
+    {
+        public string TeamName { get; set; }
+
+        private readonly List<Player> _players = new List<Player>();
+        public IEnumerable<Player> Players => new List<Player>(_players);
+
+        public Roster(string teamName, IEnumerable<Player> players)
+        {
+            TeamName = teamName;
+            _players.AddRange(players);
+        }
+    }
+}
