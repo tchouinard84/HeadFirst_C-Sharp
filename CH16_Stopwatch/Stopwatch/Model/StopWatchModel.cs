a6ca321f853c94fbff39176b7aa96721fa948863
tchouina <timothy.chouinard@emp-corp.com>
2018-01-23 14:44:43 -0500

    CH16_Stopwatch - added the StopWatchModel

---
+++ CH16_Stopwatch/Stopwatch/Model/StopWatchModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Stopwatch.Model
+{
+    public class StopwatchModel
+    {
+        private DateTime? _started;
+        private TimeSpan? _previousElapsedTime;
+
+        public bool Running => _started.HasValue;
+
+        public TimeSpan? Elapsed
+        {
+            get
+            {
+                if (!_started.HasValue) { return _previousElapsedTime; }
+                if (!_previousElapsedTime.HasValue) { return CalculateTimeElapsedSinceStarted(); }
+                return CalculateTimeElapsedSinceStarted() + _previousElapsedTime;
+            }
+        }
+
+        public StopwatchModel()
+        {
+            Reset();
+        }
+
+        public void Start()
+        {
+            _started = DateTime.Now;
+            if (_previousElapsedTime.HasValue) { return; }
+            _previousElapsedTime = new TimeSpan(0);
+        }
+
+        public void Stop()
+        {
+            if (!_started.HasValue) { return; }
+            _previousElapsedTime += DateTime.Now - _started.Value;
+            _started = null;
+        }
+
+        public void Reset()
+        {
+            _previousElapsedTime = null;
+            _started = null;
+        }
+
+        private TimeSpan CalculateTimeElapsedSinceStarted()
+        {
+            return DateTime.Now - _started.Value;
+        }
+    }
+}
