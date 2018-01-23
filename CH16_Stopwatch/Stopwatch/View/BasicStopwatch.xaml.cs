abc3aa4d26dca0a19fbb27ec0e286f63b96a6878
tchouina <timothy.chouinard@emp-corp.com>
2018-01-23 15:21:29 -0500

    CH16_Stopwatch - Added BasicStopwatch as a UserControl to the view. Added StopwatchViewModel.

---
+++ CH16_Stopwatch/Stopwatch/View/BasicStopwatch.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stopwatch.View
+{
+    /// <summary>
+    /// Interaction logic for BasicStopwatch.xaml
+    /// </summary>
+    public partial class BasicStopwatch : UserControl
+    {
+        public BasicStopwatch()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LapButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CH16_Stopwatch/Stopwatch/ViewModel/StopwatchViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Stopwatch.ViewModel
+{
+    public class StopwatchViewModel
+    {
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+        public int LapHours { get; private set; }
+        public int LapMinutes { get; private set; }
+        public int LapSeconds { get; private set; }
+    }
+}
