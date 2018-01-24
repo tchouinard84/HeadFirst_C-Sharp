3d424a0986d5ea2f9f47167edf5d7358d4dc36c1
tchouina <timothy.chouinard@emp-corp.com>
2018-01-24 12:55:35 -0500

    CH16_Stopwatch - added the TimeNumberFormatConverter

---
+++ CH16_Stopwatch/Stopwatch/ViewModel/TimeNumberFormatConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Stopwatch.ViewModel
+{
+    public class TimeNumberFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal) { return ((decimal) value).ToString("00.00"); }
+            if (!(value is int)) { return value; }
+            if (parameter == null) { return ((int) value).ToString("d1"); }
+            return ((int) value).ToString(parameter.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
