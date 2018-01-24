5eb70d3e83a22209fb49552f6d6654bcca577fe0
tchouina <timothy.chouinard@emp-corp.com>
2018-01-24 08:09:22 -0500

    CH16_Stopwatch - wrote the StopwatchViewModel

--- CH16_Stopwatch/Stopwatch/ViewModel/StopwatchViewModel.cs
+++ CH16_Stopwatch/Stopwatch/ViewModel/StopwatchViewModel.cs
@@ -1,12 +1,106 @@
-﻿namespace Stopwatch.ViewModel
+﻿using Stopwatch.Model;
+using System;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace Stopwatch.ViewModel
 {
-    public class StopwatchViewModel
+    public class StopwatchViewModel : INotifyPropertyChanged
     {
-        public int Hours { get; private set; }
-        public int Minutes { get; private set; }
-        public int Seconds { get; private set; }
-        public int LapHours { get; private set; }
-        public int LapMinutes { get; private set; }
-        public int LapSeconds { get; private set; }
+        private readonly StopwatchModel _stopwatchModel = new StopwatchModel();
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+
+        private int _lastHours;
+        private int _lastMinutes;
+        private decimal _lastSeconds;
+
+        private int _lastLapHours;
+        private int _lastLapMinutes;
+        private decimal _lastLapSeconds;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Hours => _stopwatchModel.Elapsed.HasValue ? _stopwatchModel.Elapsed.Value.Hours : 0;
+        public int Minutes => _stopwatchModel.Elapsed.HasValue ? _stopwatchModel.Elapsed.Value.Minutes : 0;
+        public decimal Seconds => _stopwatchModel.Elapsed.HasValue
+            ? _stopwatchModel.Elapsed.Value.Seconds + _stopwatchModel.Elapsed.Value.Milliseconds * .001M
+            : 0.0M;
+
+        public int LapHours => _stopwatchModel.LapTime.HasValue ? _stopwatchModel.LapTime.Value.Hours : 0;
+        public int LapMinutes => _stopwatchModel.LapTime.HasValue ? _stopwatchModel.LapTime.Value.Minutes : 0;
+        public decimal LapSeconds => _stopwatchModel.LapTime.HasValue
+            ? _stopwatchModel.LapTime.Value.Seconds + _stopwatchModel.LapTime.Value.Milliseconds * .001M
+            : 0.0M;
+
+        public bool Running => _stopwatchModel.Running;
+
+        public StopwatchViewModel()
+        {
+            _timer.Interval = TimeSpan.FromMilliseconds(50);
+            _timer.Tick += TimerTick;
+            _timer.Start();
+
+            _stopwatchModel.LapTimeUpdated += LapTimeUpdatedEventHandler;
+        }
+
+        public void Start()
+        {
+            _stopwatchModel.Start();
+        }
+
+        public void Stop()
+        {
+            _stopwatchModel.Stop();
+        }
+
+        public void Lap()
+        {
+            _stopwatchModel.Lap();
+        }
+
+        public void Reset()
+        {
+            var running = Running;
+            _stopwatchModel.Reset();
+            if (running) { _stopwatchModel.Start(); }
+            OnPropertyChanged("LapHours");
+            OnPropertyChanged("LapMinutes");
+            OnPropertyChanged("LapSeconds");
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            MaybePropertyChanged(Hours, ref _lastHours, "Hours");
+            MaybePropertyChanged(Minutes, ref _lastMinutes, "Minutes");
+            MaybePropertyChanged(Seconds, ref _lastSeconds, "Seconds");
+        }
+
+        private void LapTimeUpdatedEventHandler(object sender, LapEventArgs e)
+        {
+            MaybePropertyChanged(LapHours, ref _lastLapHours, "LapHours");
+            MaybePropertyChanged(LapMinutes, ref _lastLapMinutes, "LapMinutes");
+            MaybePropertyChanged(LapSeconds, ref _lastLapSeconds, "LapSeconds");
+        }
+
+        private void MaybePropertyChanged(int property, ref int previous, string propertyName)
+        {
+            if (previous == property) { return; }
+
+            previous = property;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void MaybePropertyChanged(decimal property, ref decimal previous, string propertyName)
+        {
+            if (previous == property) { return; }
+
+            previous = property;
+            OnPropertyChanged(propertyName);
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
