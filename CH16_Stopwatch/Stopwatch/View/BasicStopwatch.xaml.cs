5ef6146f24b9caeaeec7ffe0c127676bf16f6bf4
tchouina <timothy.chouinard@emp-corp.com>
2018-01-24 08:30:30 -0500

    CH16_Stopwatch - finished up the basic stopwatch to get it working

--- CH16_Stopwatch/Stopwatch/View/BasicStopwatch.xaml.cs
+++ CH16_Stopwatch/Stopwatch/View/BasicStopwatch.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Stopwatch.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Stopwatch.View
 {
@@ -20,29 +9,33 @@ namespace Stopwatch.View
     /// </summary>
     public partial class BasicStopwatch : UserControl
     {
+        private readonly StopwatchViewModel viewModel;
+
         public BasicStopwatch()
         {
             InitializeComponent();
+
+            viewModel = FindResource("viewModel") as StopwatchViewModel;
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            viewModel.Start();
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            viewModel.Stop();
         }
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            viewModel.Reset();
         }
 
         private void LapButton_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            viewModel.Lap();
         }
     }
 }
