4bd7a39e79553499179b1bfd93d9d10de3fd9cbe
tchouina <timothy.chouinard@emp-corp.com>
2018-01-25 16:08:51 -0500

    CH16_Stopwatch - Added another UserControl (AnalogStopwatch) to the View.

---
+++ CH16_Stopwatch/Stopwatch/View/AnalogStopwatch.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Stopwatch.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Stopwatch.View
+{
+    /// <summary>
+    /// Interaction logic for AnalogStopwatch.xaml
+    /// </summary>
+    public partial class AnalogStopwatch : UserControl
+    {
+        private readonly StopwatchViewModel viewModel;
+
+        public AnalogStopwatch()
+        {
+            InitializeComponent();
+
+            viewModel = FindResource("ViewModel") as StopwatchViewModel;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Stop();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Reset();
+        }
+
+        private void LapButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Lap();
+        }
+    }
+}
---
+++ CH16_Stopwatch/Stopwatch/ViewModel/AngleConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Stopwatch.ViewModel
+{
+    public class AngleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) { return 0; }
+            if (!double.TryParse(value.ToString(), out var parsedValue)) { return 0; }
+            if (parameter == null) { return 0; }
+
+            switch (parameter.ToString())
+            {
+                case "Hours":
+                    return parsedValue * 30;
+                case "Minutes":
+                case "Seconds":
+                    return parsedValue * 6;
+                default:
+                    return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
