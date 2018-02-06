4b4c72ba9631281bb9e70ca6efbae81ab0bd429c
tchouina <timothy.chouinard@emp-corp.com>
2018-02-06 13:40:25 -0500

    CH16_Stopwatch - moved the code for the 4 hands on the analog stopwatch from XAML to C# code

--- CH16_Stopwatch/Stopwatch/View/AnalogStopwatch.xaml.cs
+++ CH16_Stopwatch/Stopwatch/View/AnalogStopwatch.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Stopwatch.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -19,6 +20,7 @@ namespace Stopwatch.View
 
             _viewModel = FindResource("ViewModel") as StopwatchViewModel;
             AddMarkings();
+            AddHands();
         }
 
         private void AddMarkings()
@@ -32,11 +34,7 @@ namespace Stopwatch.View
         {
             for (var i = 0.0; i < 360.0; i += angleBetween)
             {
-                var rectangle = new Rectangle();
-                rectangle.Width = width;
-                rectangle.Height = height;
-                rectangle.Fill = new SolidColorBrush(Colors.Black);
-                rectangle.RenderTransformOrigin = new Point(0.5, 0.5);
+                var rectangle = Rectangle(width, height, new SolidColorBrush(Colors.Black));
 
                 var transforms = new TransformGroup();
                 transforms.Children.Add(new TranslateTransform { Y = offset });
@@ -44,6 +42,53 @@ namespace Stopwatch.View
                 rectangle.RenderTransform = transforms;
                 baseGrid.Children.Add(rectangle);
             }
+        }
+
+        private void AddHands()
+        {
+            AddHand(1.5, 230, -100, new SolidColorBrush(Colors.Black), "Seconds", "Seconds");
+            AddHand(3, 175, -75, new SolidColorBrush(Colors.Black), "Minutes", "Minutes");
+            AddHand(.75, 230, -100, new SolidColorBrush(Colors.Yellow), "LapSeconds", "Seconds");
+            AddHand(1.5, 175, -75, new SolidColorBrush(Colors.Yellow), "LapMinutes", "Minutes");
+        }
+
+        private void AddHand(double width, double height, double offset, Brush color, string property, string converterParameter)
+        {
+            var transforms = new TransformGroup();
+            transforms.Children.Add(new TranslateTransform { Y = offset });
+            transforms.Children.Add(RotateTransform(property, converterParameter));
+            baseGrid.Children.Add(Rectangle(width, height, color, transforms));
+        }
+
+        private static Rectangle Rectangle(double width, double height, Brush color, Transform transforms = null)
+        {
+            return new Rectangle
+            {
+                Width = width,
+                Height = height,
+                Fill = color,
+                RenderTransformOrigin = new Point(0.5, 0.5),
+                RenderTransform = transforms
+            };
+        }
+
+        private RotateTransform RotateTransform(string property, string converterParameter)
+        {
+            var rotateTransform = new RotateTransform();
+            BindingOperations.SetBinding(rotateTransform, System.Windows.Media.RotateTransform.AngleProperty,
+                BindingFor(property, converterParameter));
+            return rotateTransform;
+        }
+
+        private Binding BindingFor(string property, string converterParameter)
+        {
+            return new Binding()
+            {
+                Source = _viewModel,
+                Path = new PropertyPath(property),
+                Converter = new AngleConverter(),
+                ConverterParameter = converterParameter
+            };
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
